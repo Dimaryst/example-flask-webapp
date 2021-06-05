12b506be6d1a3e3141189324f287c6f62346c602
Dimaryst <dimaryst@icloud.com>
2021-06-05 15:50:18 +0700

    hi page added

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/hi_page')
+def hi():
+    return "<h1>HI! My dear friend!</h1>"
 
 
 if __name__ == '__main__':
